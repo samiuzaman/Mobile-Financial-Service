bd8add19442910a8a3f974f4f90b84daf3bfc823
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-08 23:44:27 +0600

    Create Login page and add login functionality

--- login-script.js
+++ login-script.js
@@ -1,2 +1,17 @@
+// Login Functionality
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  let mobileNumber = document.getElementById("mobile-number").value;
+  let pin = document.getElementById("pin").value;
 
+  if (mobileNumber.length === 11) {
+    if (parseInt(mobileNumber) === 12345678910 && parseInt(pin) === 1234) {
+      document.location.href = "./home.html";
+    } else {
+      alert("Invalid User Info");
+    }
+  } else {
+    alert("Need a valid account number");
+  }
+});
 
